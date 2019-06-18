396f458d73bdbe8155a1483d756ae71c170a0683
kongdz@yonyou.com <kongdz@yonyou.com>
2019-06-18 14:30:37 +0800

    提交添加安全认证的高可用eureka注册中心代码

---
+++ cloud-parent/eureka-server/src/main/java/com/springcloudlearning/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.springcloudlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka-server eureka注册中心启动类
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
